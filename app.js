57aff230414b3f562a992e7449701d363f319128
TamilTamilarasan <138966974+TamilTamilarasan@users.noreply.github.com>
2023-07-12 16:13:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var image =document.getElementById("imge");
 
 function blub_on(){
-    image.src="./image/bulb on.jpg";
+    image.src="./bulb on.jpg";
 }
 function blub_off(){
-    image.src="./image/bulb off.jpg";
+    image.src="./bulb off.jpg";
 }
